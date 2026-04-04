60750f1e37016ab279dfcbea3dacca9d1ead0ca0
startzpranjal <startzpranjalrajput999@gmail.com>
2026-04-04 14:37:18 +0530

    Update teryxpanel.sh

--- teryxpanel.sh
+++ teryxpanel.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-figlet STARTZ && figlet TERYX PANEL && git clone https://github.com/teryxlabs/v4panel && curl -sL https://deb.nodesource.com/setup_23.x | sudo bash - && apt-get install nodejs git && cd v4panel && apt install zip -y && unzip panel.zip && npm install && npm run seed && npm run createUser && node .
+figlet STARTZ && figlet TERYX PANEL && git clone https://github.com/teryxlabs/v4panel && curl -sL https://deb.nodesource.com/setup_23.x | sudo bash - && apt-get install nodejs && cd v4panel && apt install zip -y && unzip panel.zip && npm install && npm run seed && npm run createUser && node .
