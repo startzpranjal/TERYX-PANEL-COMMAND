925620f696d59ecdc50ea95ffa4c82eaf41c9706
startzpranjal <startzpranjalrajput999@gmail.com>
2026-04-04 20:07:19 +0530

    Update teryxdaemon.sh

--- teryxdaemon.sh
+++ teryxdaemon.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-figlet STARTZ && figlet TERYX DAEMON && git clone https://github.com/dragonlabsdev/daemon && cd daemon && apt install zip -y && unzip daemon.zip && cd daemon && npm install && figlet CONFIGURE YOUR NODE CONFIGRATION AND PASTE IT IN TERMINAL AND TYPE 'node .' IN THE TERMINAL
+figlet STARTZ && figlet TERYX DAEMON && git clone https://github.com/dragonlabsdev/daemon && cd daemon && apt install zip -y && unzip daemon.zip && cd daemon && npm install && figlet CONFIGURE YOUR NODE CONFIGRATION AND PASTE IT IN TERMINAL AND TYPE 'node .' IN THE TERMINAL && cd daemon 
