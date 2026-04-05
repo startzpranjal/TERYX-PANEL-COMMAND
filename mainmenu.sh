3e45e074a53283fdd34136e9021a71d472ca652f
startzpranjal <startzpranjalrajput999@gmail.com>
2026-04-05 19:14:08 +0530

    Update mainmenu.sh

--- mainmenu.sh
+++ mainmenu.sh
@@ -7,10 +7,7 @@ CYAN='\033[0;36m'
 YELLOW='\033[1;33m'
 NC='\033[0m'
 
-while true; do
-    clear
-
-    # Title
+draw_menu() {
     echo -e "${CYAN}"
     figlet -f big "STARTZ MAIN MENU"
     echo -e "${NC}"
@@ -20,7 +17,12 @@ while true; do
     echo -e "${GREEN}2.${NC} Install Teryx Daemon/Node"
     echo -e "${RED}0.${NC} Exit"
     echo -e "${YELLOW}=================================${NC}"
+}
 
+clear
+draw_menu
+
+while true; do
     echo -ne "Enter your choice: "
     read choice
 
@@ -28,19 +30,21 @@ while true; do
         1)
             echo -e "${CYAN}Installing Teryx Panel...${NC}"
             bash -c "$(curl -sSL https://raw.githubusercontent.com/startzpranjal/TERYX-PANEL-COMMAND/refs/heads/main/teryxpanel.sh)"
-            read -p "Press Enter to return to menu..."
+            echo
+            draw_menu
             ;;
         2)
             echo -e "${CYAN}Installing Teryx Daemon/Node...${NC}"
             bash -c "$(curl -sSL https://raw.githubusercontent.com/startzpranjal/TERYX-PANEL-COMMAND/refs/heads/main/teryxdaemon.sh)"
-            read -p "Press Enter to return to menu..."
+            echo
+            draw_menu
             ;;
         0)
             echo -e "${RED}Exiting...${NC}"
             exit 0
             ;;
         *)
-            # silent refresh
+            # do nothing, just re-prompt without redraw
             ;;
     esac
 done
